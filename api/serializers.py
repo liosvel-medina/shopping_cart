3afaccdfd4c48efad707f88a5b8df65a84261954
Liosvel Medina Molina <liosvel.pepey@gmail.com>
2023-05-16 02:10:09 -0400

    add nested serializers

--- api/serializers.py
+++ api/serializers.py
@@ -3,19 +3,40 @@ from rest_framework import serializers
 from api import models
 
 
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Color
+        fields = '__all__'
+
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Brand
+        fields = '__all__'
+
+
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Cap
+        model = models.Product
         exclude = ['initial_stock']
 
 
 class CapListSerializer(serializers.ModelSerializer):
+    main_color = ColorSerializer(many=False, read_only=True)
+    secondary_colors = ColorSerializer(many=True, read_only=True)
+    brand = BrandSerializer(many=False, read_only=True)
+    logo_color = ColorSerializer(many=False, read_only=True)
+
     class Meta:
         model = models.Cap
         exclude = ['initial_stock']
 
 
 class TShirtListSerializer(serializers.ModelSerializer):
+    main_color = ColorSerializer(many=False, read_only=True)
+    secondary_colors = ColorSerializer(many=True, read_only=True)
+    brand = BrandSerializer(many=False, read_only=True)
+
     class Meta:
         model = models.TShirt
         exclude = ['initial_stock']
